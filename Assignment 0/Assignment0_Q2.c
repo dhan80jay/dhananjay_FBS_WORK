687b1491e21c6d49317af6933d92fe4a1d5b12dc
dhan80jay <dhananjaysangle2005@gmail.com>
2025-05-31 10:37:27 +0530

    Add files via upload

---
+++ Assignment 0/Assignment0_Q2.c
@@ -0,0 +1,9 @@
+// Q.2 Calculate percentage of student 
+void main ()
+{
+float sub1=36,sub2=55,sub3=56,sub4=65,sub5=65;
+float total;
+total=sub1+sub2+sub3+sub4+sub5;
+float percentage = total/500*100;
+printf("Percentage of student is %f",percentage);
+}
